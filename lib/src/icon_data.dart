d8ce26c70d05bb1d373aeac58e333e99732b164f
Regendra <regendrasuman2017@gmail.com>
2024-01-05 16:30:06 +0530

    add

---
+++ lib/src/icon_data.dart
@@ -0,0 +1,14 @@
+library boxing_img;
+
+import "package:flutter/widgets.dart";
+
+class BoxingIconsData extends IconData {
+  const BoxingIconsData(int codePoint)
+      : super(
+          codePoint,
+          fontFamily: "BoxingIcons",
+          fontPackage: "boxing_img",
+        );
+}
+
+
